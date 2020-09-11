eb7fc158348befecd0a1bc813fa49dbc5cbd18a8
PJ <pjjoh1@gmail.com>
2020-09-11 15:16:28 +1000

    New search by email/name and list rooms

--- webex_teams_api.py
+++ webex_teams_api.py
@@ -2,7 +2,11 @@ import requests
 import json
 import pprint
 
-# PJ Version
+#------------------------------------
+# PJ Version - pajohns3@cisco.com
+# for Anyone Can Code Week 4
+#------------------------------------
+
 #Disable annoying warnings
 import urllib3
 urllib3.disable_warnings()
@@ -14,18 +18,38 @@ BOT_TOKEN = CRED.TOKEN
 
 
 #Start of functions
-def list_people_email():
+def list_people_email(emailaddr):
 	''' List people by email '''
 
-	# <Add your code here>
+# PJ
+
+	url = 'https://webexapis.com/' + '/v1/people'
+
+	headers = {'Authorization': 'Bearer ' + BOT_TOKEN}
+
+	payload = {'email' : emailaddr}
+	
+	response = requests.get(url, headers=headers, params=payload, verify=False)
+
+	pprint.pprint(response.json())
 
 
 
-def list_people_displayname():
+
+def list_people_displayname(dispname):
 	''' List people by display name '''
 
-	# <Add your code here>
+# PJ
 
+	url = 'https://webexapis.com/' + '/v1/people'
+
+	headers = {'Authorization': 'Bearer ' + BOT_TOKEN}
+
+	payload = {'displayName' : dispname}
+	
+	response = requests.get(url, headers=headers, params=payload, verify=False)
+
+	pprint.pprint(response.json())
 
 
 def get_person_detail_ID():
@@ -51,7 +75,17 @@ def me():
 def list_room():
 	''' List the rooms the Webex Teams bot is in '''
 
-	# <Add your code here>
+	# PJ
+
+	url = 'https://webexapis.com/' + '/v1/rooms'
+
+	headers = {'Authorization': 'Bearer ' + BOT_TOKEN}
+	
+	response = requests.get(url, headers=headers, verify=False)
+
+	pprint.pprint(response.json())
+
+	return response
 
 
 
@@ -64,8 +98,17 @@ def create_room():
 
 def get_rooms():
 	''' List the rooms the Webex Teams bot is in '''
+	# PJ
 
-	# <Add your code here>
+	url = 'https://webexapis.com/' + '/v1/rooms'
+
+	headers = {'Authorization': 'Bearer ' + BOT_TOKEN}
+	
+	response = requests.get(url, headers=headers, verify=False)
+
+	pprint.pprint(response.json())
+
+	return response
 
 
 
@@ -111,4 +154,14 @@ def delete_message():
 
 if __name__ == '__main__':
 
-	me()
+	#me()
+	#get_rooms()
+
+	#PJ - Get People info by email address
+	#mail_input = input("Search user by email, enter email address: ")
+	#ist_people_email(email_input)
+
+	#PJ - Get People info by email address
+	dispname = input("Search user by name, enter name: ")
+	list_people_displayname(dispname)
+	
